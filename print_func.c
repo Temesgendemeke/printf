07d99366d696eb9ecf1225b78fd64eb959a6342e
temesgendemeke <temesgen100k@gmail.com>
2023-09-19 10:17:57 -0700

    print_func.c

---
+++ print_func.c
@@ -0,0 +1,59 @@
+#include "main.h"
+
+/**
+ * print_c - prints char
+ * @args: args
+ * Return: no of chars printed
+*/
+
+int print_c(va_list args)
+{
+	char c = (char)va_arg(args, int);
+
+	_putchar(c);
+	return (1);
+}
+
+/**
+ * print_s - prints string
+ * @args: args
+ * Return: no of printed chars
+*/
+
+int print_s(va_list args)
+{
+	int i;
+	char *str = va_arg(args, char *);
+
+	if (str == NULL)
+		str = "(null)";
+	for (i = 0; str[i] != '\0'; i++)
+		_putchar(str[i]);
+	return (i);
+}
+
+/**
+ * print_perc - prints percent
+ * @args: arguments
+ * Return: no of chars printed
+*/
+
+int print_perc(__attribute__((unused))va_list args)
+{
+	_putchar('%');
+	return (1);
+}
+
+/**
+ * print_int - prints integer
+ * @args: the arguments
+ * Return: the length
+*/
+
+int print_int(va_list args)
+{
+	int length;
+
+	length = print_nums(args);
+	return (length);
+}
