a3d9ac4b33c0bc5d93781f1fe90a00f2dda09224
meta-bay <bayiemeseret@gmail.com>
2023-09-20 02:09:35 +0300

    commiting to printf

--- _printf.c
+++ _printf.c
@@ -16,6 +16,7 @@ int _printf(const char *format, ...)
 			{"o", print_oct}, {"x", print_hexa},
 			{"X", print_heXA}, {"u", print_un_int},
 			{"r", print_rev}, {"R", rot13},
+			{"S", nonPrintHex},
 			{NULL, NULL}};
 	va_list args;
 
